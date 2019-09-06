d891f2479e01fadc988a7db947f39cfb92462844
SandeepVattapparambil <sandeepv68@gmail.com>
2019-09-07 02:49:10 +0530

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+const geoCode = require("../services/geocode");
+
 /**
  * Index route
  */
 router.get("/", function(req, res, next) {
-  let reponse = {
-    success: true,
-    status: "OK",
-    message: "markit-server is successfully"
-  };
+  let response = {};
+  geoCode
+    .getGeoCode("berlin")
+    .then(function(res) {
+      reponse = {
+        success: true,
+        status: "OK",
+        message: "markit-server is successfully",
+        data: Object.assign({}, ...res).formatted_address
+      };
+    })
+    .catch(function(err) {
+      reponse = {
+        success: false,
+        status: "OK",
+        message: "markit-server error"
+      };
+    });
+
   res.status(200);
   res.json(reponse);
 });
---
+++ services/geocode.js
@@ -0,0 +1,21 @@
+const GoogleMaps = require("@google/maps");
+
+let googleMapsConfig = require("../config/googleMapsConfig.json");
+const googleMapsClient = GoogleMaps.createClient({
+  key: googleMapsConfig.api_key,
+  Promise: Promise
+});
+const geoCodeLib = {};
+geoCodeLib.getGeoCode = query => {
+  return googleMapsClient
+    .geocode({ address: query })
+    .asPromise()
+    .then(response => {
+      return response.json.results;
+    })
+    .catch(err => {
+      return err;
+    });
+};
+
+module.exports = geoCodeLib;
